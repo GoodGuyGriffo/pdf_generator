c7113f9550eb6ff83d7c5b01a5c5305b50a000cc
Griffo <griffo2912@gmail.com>
2022-12-21 18:34:51 +1100

    final

--- main.py
+++ main.py
@@ -13,4 +13,12 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
     pdf.line(10, 21, 200, 21)
 
+    # pdf.ln(260)
+    # pdf.set_font(family="Times", style="I", size=8)
+    # pdf.set_text_color(180, 180, 180)
+    # pdf.cell(w=0, h=0, txt=row["Topic"], align="R")
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+
 pdf.output("output.pdf")
